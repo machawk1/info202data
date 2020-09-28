6fd2a2427b7365d12963676441ce55ed4b5bfae5
Ed Summers <ehs@pobox.com>
2020-09-27 21:32:21 -0400

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'inst341data',
-    version = '0.0.3',
+    version = '0.0.4',
     author = 'Ed Summers',
     author_email = 'ehs@pobox.com',
     url = 'https://github.com/edsu/inst341data',
